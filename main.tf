9c26dd49319d591e32618a26f48e259710b70b60
BabakDoraniarab <babakdoraniarab@jajafinance.com>
2022-12-28 14:47:57 +0200

    new module is added

---
+++ main.tf
@@ -0,0 +1,34 @@
+variable "infra_data_organization" {}
+variable "infra_data_workspace" {}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = var.infra_data_organization
+    workspaces {
+      name = var.infra_data_workspace
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.DEV_ACCESS_KEY_ID
+  secret_key = var.DEV_SECRET_ACCESS_KEY
+}
+
+
+module "s3-module" {
+  source  = "app.terraform.io/JAJA-DATA-DEV/s3-module/aws"
+  version = "1.0.0"
+  # insert required variables here
+  bucket_name   = "babak-bucket"
+  bucket_region = "eu-west-1"
+}
---
+++ var.tfvars
@@ -0,0 +1,3 @@
+infra_data_organization = "JAJA-DATA-DEV"
+infra_data_workspace    = "s3-terraform-cloud"
+region                  = "eu-west-1"
